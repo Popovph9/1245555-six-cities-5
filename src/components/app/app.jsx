a518ec1cdec5f49d823c40d35a75af7da40ec03e
Popovph9 <58945032+Popovph9@users.noreply.github.com>
2020-10-02 22:21:57 +0300

    Выполнено задание 7 личного проекта: маршрут по тайным тропам (#4)
    Co-authored-by: philipp <popovph9@gmil.com>

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import MainPage from "../main-page/main-page";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import MainPage from "../main-page/main-page";
+import LoginScreen from "../login-screen/login-screen";
+import Favorites from "../favorites/favorites";
+import OfferScreen from "../offer-screen/offer-screen";
 
 const App = (props) => {
   const {offersCount} = props;
 
   return (
-    <MainPage
-      offersCount = {offersCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage offersCount = {offersCount}/>
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites/>
+        </Route>
+        <Route exact path="/offer/:id?">
+          <OfferScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
